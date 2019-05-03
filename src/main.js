079e708f45199a30fe05a7e02cd7329e2de40448
Kongpheng Nanthavongsa <Kongpheng.Nanthavongsa@vonage.com>
2019-05-03 11:21:17 -0700

    Don't capture video and sound; only capture sound

--- src/main.js
+++ src/main.js
@@ -1,29 +1,50 @@
-class Media {
-  constructor() {
-    const hasGetUserMedia =
-          !!(navigator.mediaDevices &&
-             navigator.mediaDevices.getUserMedia);
+class SpeechRecognition {
+  constructor(opts) {
+    const api = window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition;
 
-    if (!hasGetUserMedia) {
-      alert('Your browser does not have getUserMedia() support');
+    if (!api) {
+      throw new Error("This browser doesn't support the Speech Recognition API");
     }
 
-    this.constraints = {
-      video: true,
-      audio: true
+    this.api = new api();
+  }
+
+  // Brings up the "Allow microphone access" dialog and starts the SR engine
+  start(opts = {}) {
+    const defaultOpts = {
+      // Language Speech Recognition engine (SR) should understand
+      // If not specified, then it will default to the language the user agent
+      // (i.e., browser) is set to
+      lang: 'en-US',
+
+      interimResults: false,
+
+      // Keep listening and transcribing as long as the app is open
+      continuous: true,
+
+      onresult: this.onresult,
     };
+
+    const mergedOptions = Object.assign(defaultOpts, opts);
+
+    Object.keys(mergedOptions).forEach(opt => {
+      this.api[opt] = mergedOptions[opt];
+    }, this);
+
+    this.api.start();
   }
-  stream(element) {
-    const { constraints } = this;
 
-    navigator.mediaDevices.getUserMedia(constraints).
-      then((stream) => {
-        element.srcObject = stream;
-      });
+  onresult(event) {
+    // Index of the most recent utterance that was understood
+    const { resultIndex } = event;
+
+    // Parse the most recent utterance
+    const result = event.results[resultIndex];
+    const transcript = result[0].transcript;
+
+    console.log(transcript);
   }
-};
+}
 
-const media = new Media();
-const videoContainer = document.getElementById('video');
-
-media.stream(videoContainer);
+const sr = new SpeechRecognition();
+sr.start();
